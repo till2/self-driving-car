a324d64eb16f7a8bbe651e101e19c629c08f99d7
till2 <89709351+till2@users.noreply.github.com>
2023-07-02 14:26:57 +0200

    path debugging 2

--- tests/test_src.py
+++ tests/test_src.py
@@ -10,7 +10,8 @@ import sys
 from collections import deque
 from operator import itemgetter
 
-sys.path.insert(1, '..')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 print(sys.path)
 
 import gym_donkeycar
