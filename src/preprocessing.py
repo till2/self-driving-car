6d5ca651e3826f7fd623905a4a5313c632c250ce
till2 <89709351+till2@users.noreply.github.com>
2023-07-08 16:33:22 +0200

    change preprocessing so that it works with batches (needs channel-dim first)

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -11,7 +11,7 @@ grayscale = config["grayscale"]
 height, width = config["size"]
 
 """ 
-transform: scales to [0,1], resizes to [H_new,W_new] and optionally grayscales a batch of images
+Scales to [0,1], resizes to [H_new,W_new] and optionally grayscales a batch of images
 
 Args:
     x (torch.Tensor): input tensor => use torch.tensor(obs) as the input
@@ -26,10 +26,12 @@ Notes:
 """
 transform = transforms.Compose([
     #transforms.ToTensor(), # devides by 255 to scale the input to the range [0,1]
+    transforms.Lambda(lambda x: x.permute(0, 3, 1, 2)), # => (B,C,H,W)
     transforms.Lambda(lambda x: x / 255),
     transforms.Lambda(lambda x: x.to(device)),
     transforms.Resize((height, width)),
     transforms.Grayscale() if grayscale else transforms.Lambda(lambda x: x),
+    transforms.Lambda(lambda x: x.permute(0, 2, 3, 1)),
 ])
 
 # result = preprocess(obs)
